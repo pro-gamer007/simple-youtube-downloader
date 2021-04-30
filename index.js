2ddb13b1b2fcb89b3968b6da66bb58a5f225a6ff
pro_gamer007 <66844505+pro-gamer007@users.noreply.github.com>
2021-04-30 19:22:56 +0530

    Back to old ways?

--- index.js
+++ index.js
@@ -30,11 +30,10 @@ app.on('window-all-closed', () => {
 });
 
 ipcMain.on('link', async(event, arg) => {
-	const video = ytdl(arg)
 	const { filePath } = await dialog.showSaveDialog({
 	    defaultPath: 'output.mp4'
   	});
   	if (filePath) {
-    		fs.writeFile(filePath, video, () => console.log(`[${Date.now}] video saved.`));
+		ytdl(arg).pipe(fs.createWriteStream(filepath));
   	}
 });
